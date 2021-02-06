0f93ce1a19e9e0c3f99180eea3d992bef4219ab0
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-06 14:39:57 -0400

    adding Provider and DefaultTheme from react-native-paper

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
+
 import Routes from '../infra/routes';
 
 const App: React.FC = () => {
+  const theme = {
+    ...DefaultTheme,
+    roundness: 15,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#3498db',
+      accent: '#f1c40f',
+    },
+  };
   return (
     <NavigationContainer>
-      <Routes />
+      <PaperProvider theme={theme}>
+        <Routes />
+      </PaperProvider>
     </NavigationContainer>
   )
 }
