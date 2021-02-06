407a4454801b15211336bea70e7fe96776cb9c7b
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-06 19:44:41 -0400

    chore: adding tab navigator

--- src/infra/routes/signed.routes.tsx
+++ src/infra/routes/signed.routes.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
+Feather.loadFont();
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+MaterialIcons.loadFont();
 
-import { createStackNavigator } from '@react-navigation/stack';
+import Post from '../../presentation/pages/Post';
+import Profile from '../../presentation/pages/Profile';
+import Album from '../../presentation/pages/Album';
 
-import Login from '../../presentation/pages/Login';
+const Tab = createBottomTabNavigator();
 
-const Stack = createStackNavigator();
-
-
-
-const StackNavigator: React.FC = () => {
+const Routes: React.FC = () => {
   return (
-    <Stack.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-      })}
-    >
-      <Stack.Screen name="Login" component={Login} />
-    </Stack.Navigator>
-  )
-};
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
 
-export default StackNavigator;
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Post') {
+              iconName = focused ? 'message-square' : 'message-square';
+            } else if (route.name === 'Album') {
+              iconName = focused
+                ? 'image'
+                : 'image';
+            } else {
+              iconName = focused
+                ? 'user'
+                : 'user';
+            }
+            return <Feather name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeBackgroundColor: '#40657A',
+          inactiveBackgroundColor: '#16394D',
+          activeTintColor: '#FFFFFF',
+          inactiveTintColor: '#FFFFFF',
+        }}>
+        <Tab.Screen name="Post" component={Post} />
+        <Tab.Screen name="Album" component={Album} />
+        <Tab.Screen name="Perfil" component={Profile} />
+      </Tab.Navigator>
+  )
+}
+
+export default Routes;
