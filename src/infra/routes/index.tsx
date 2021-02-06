9202b12fb4afb9f61ca32a4f33109de89bf46fc5
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-06 16:36:57 -0400

    fix: adapting routes to logged and unlogged status

--- src/infra/routes/index.tsx
+++ src/infra/routes/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Sign from './sign.routes';
 import Signed from './signed.routes';
 
+import AuthContext from '../../data/contexts/Auth';
+
 const Routes: React.FC = () => {
+  const { signed } = useContext(AuthContext);
   return (
-    <Sign />
+    signed ? <Signed /> : <Sign />
   )
 }
 
--- src/infra/routes/sign.routes.tsx
+++ src/infra/routes/sign.routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Login from '../../presentation/Login';
+import Login from '../../presentation/pages/Login';
 
 const Stack = createStackNavigator();
 
