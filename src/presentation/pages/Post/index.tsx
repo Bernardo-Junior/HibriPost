dc4e86689e3b4ee922c8c298a9be27758339c38c
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-07 16:02:23 -0400

    chore: adding error component in screen

--- src/presentation/pages/Post/index.tsx
+++ src/presentation/pages/Post/index.tsx
@@ -4,11 +4,15 @@ import api from '../../../infra/services/api';
 
 import Header from '../../components/Header';
 
+import Error from '../../components/Error';
+
 import { IPost } from '../../../data/protocols/Post';
 
 import { Alert, ListRenderItem } from 'react-native';
 
 import ModalPostContext from '../../../data/contexts/ModalPost';
+
+import ErrorContext from '../../../data/contexts/Error';
 
 import ModalPost from '../../components/ModalPost';
 
@@ -30,28 +34,40 @@ import {
   BtnLeft,
   IconLeft,
   IconRight,
-  TxtPage
+  TxtPage,
+  RefreshControl
 } from './styles';
 
 const Post: React.FC = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
   const [copyPosts, setCopyPosts] = useState<IPost[]>([]);
   const [valueSearch, setValueSearch] = useState<string>("");
-  const {  setPost, setStateModal } = useContext(ModalPostContext);
+  const { setPost, setStateModal } = useContext(ModalPostContext);
+  const { pressTry, setPressTry } = useContext(ErrorContext);
   const [page, setPage] = useState<number>(1);
   const [stateBtnLeft, setStateBtnLeft] = useState<boolean>(true);
-  const [stateBtnRight, setStateBtnRight] = useState<boolean>(false); 
+  const [stateBtnRight, setStateBtnRight] = useState<boolean>(false);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  const [con, setCon] = useState<Boolean>(true);
 
   useEffect(() => {
-    loadData();
-    statePage();
+      loadData();
+      statePage();
   }, [page])
 
+  useEffect(() => {
+    console.log(pressTry);
+    if(pressTry) {
+      loadData();
+      statePage();
+    }
+  }, [pressTry])
+
   const statePage = () => {
-    if(page === 1) {
+    if (page === 1) {
       setStateBtnLeft(true)
     }
-    else if(page === 10) {
+    else if (page === 10) {
       setStateBtnRight(true);
     }
     else {
@@ -63,13 +79,17 @@ const Post: React.FC = () => {
   const loadData = useCallback(() => {
     api.get(`/posts?userId=${page}`)
       .then(response => {
-        if(response.data != null){
+        if (response.data != null) {
+          setPressTry(false);
           setPosts(response.data);
+          setCon(true);
           setCopyPosts(response.data);
-        } 
+        }
       })
       .catch(error => {
-        Alert.alert("Ocorreu um erro", error);
+        setPressTry(false);
+        setRefreshing(false);
+        setCon(false);
       })
   }, [page])
 
@@ -87,7 +107,7 @@ const Post: React.FC = () => {
             : `${item.body.substring(0, 90)}...`}"
         </TxtInfocard>
         <ViewBtnMore>
-          <BtnMore onPress={() => {setStateModal(true), setPost(item)}}>
+          <BtnMore onPress={() => { setStateModal(true), setPost(item) }}>
             <IconArrow name="arrow-right-circle" size={35} />
           </BtnMore>
         </ViewBtnMore>
@@ -95,44 +115,64 @@ const Post: React.FC = () => {
     )
   }
 
+  function _onRefresh() {
+    setRefreshing(true);
+    loadData();
+  }
+
   return (
     <>
       <ModalPost />
       <StatusBar backgroundColor={"#E5E5E5"} />
       <Container>
-        <ScroolView nestedScrollEnabled={true}>
-          <Header />
-          <SearchBarPost
-            placeholder="Search"
-            onChangeText={value => { setValueSearch(String(value)) }}
-            value={valueSearch}
-          />
-
-          <ViewPagination>
-            <BtnLeft 
-              disabled={stateBtnLeft} 
-              state={stateBtnLeft}
-              onPress={() => {setPage(page-1), setStateBtnLeft(false)}}>
-              <IconLeft name="chevron-left" size={35} />
-            </BtnLeft>
-            <TxtPage>{page}</TxtPage>
-            <BtnRight 
-              disabled={stateBtnRight}
-              state={stateBtnRight} 
-              onPress={() => {setPage(page+1), setStateBtnLeft(false)}}>
-              <IconRight name="chevron-right" size={35} />
-            </BtnRight>
-          </ViewPagination>
-          <SafeAreaView>
-            <FlatList
-              data={posts}
-              keyExtractor={posts => String(posts.id)}
-              showsVerticalScrollIndicator={false}
-              numColumns={1}
-              renderItem={renderItem}
+      {con
+            ?
+            <>
+        <ScroolView
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={_onRefresh.bind(this)}
             />
-          </SafeAreaView>
+          }
+        >
+          <Header />
+          
+              <SearchBarPost
+                placeholder="Search"
+                onChangeText={value => { setValueSearch(String(value)) }}
+                value={valueSearch}
+              />
+
+              <ViewPagination>
+                <BtnLeft
+                  disabled={stateBtnLeft}
+                  state={stateBtnLeft}
+                  onPress={() => { setPage(page - 1), setStateBtnLeft(false) }}>
+                  <IconLeft name="chevron-left" size={35} />
+                </BtnLeft>
+                <TxtPage>{page}</TxtPage>
+                <BtnRight
+                  disabled={stateBtnRight}
+                  state={stateBtnRight}
+                  onPress={() => { setPage(page + 1), setStateBtnLeft(false) }}>
+                  <IconRight name="chevron-right" size={35} />
+                </BtnRight>
+              </ViewPagination>
+              <SafeAreaView>
+                <FlatList
+                  data={posts}
+                  keyExtractor={posts => String(posts.id)}
+                  showsVerticalScrollIndicator={false}
+                  numColumns={1}
+                  renderItem={renderItem}
+                />
+              </SafeAreaView>
+            
         </ScroolView>
+        </>
+            : <Error />
+          }
       </Container>
     </>
   )
