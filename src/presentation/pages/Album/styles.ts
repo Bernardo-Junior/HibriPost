df5aa783191c55dfd5d2ae3048801431e71197ff
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-08 12:07:35 -0400

    style: adding flatlist and styling the search field

--- src/presentation/pages/Album/styles.ts
+++ src/presentation/pages/Album/styles.ts
@@ -1,8 +1,22 @@
 import styled from 'styled-components/native';
 
+import { FlatList } from 'react-native';
+
+import Searchbar from 'react-native-paper/src/components/Searchbar';
+
+import resp from '../../responsivity';
+
 export const StatusBar = styled.StatusBar``;
 
 export const Container = styled.View`
   flex: 1;
   background-color: #E5E5E5;
 `;
+
+export const SearchBarPost = styled(Searchbar)`
+  width: ${resp(350)}px;
+  align-self: center;
+  margin-bottom: ${resp(40)}px;
+`;
+
+export {FlatList};
