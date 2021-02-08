fb47002c34716a6462045fef3a045040c64d5502
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-08 17:52:27 -0400

    fix: Adding information

--- src/presentation/pages/Album/index.tsx
+++ src/presentation/pages/Album/index.tsx
@@ -1,40 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 
 import Header from '../../components/Header';
 
 import { Alert, ListRenderItem } from 'react-native';
 
+import ModalGallery from '../../components/ModalGallery';
+
+import ModaGalleryContext from '../../../data/contexts/ModalGallery';
+
 import {
   StatusBar,
   Container,
   SearchBarPost,
-  FlatList
+  FlatList,
+  ContainerCards,
+  Cards,
+  ScroolView,
+  TxtTitle,
+  BtnCards,
+  RefreshControl,
+  BtnLeft,
+  BtnRight,
+  IconLeft,
+  IconRight,
+  TxtPage,
+  ViewPagination
 } from './styles';
+
+import Loading from '../../components/Loading';
 
 import { IAlbum } from '../../../data/protocols/Album';
 
 import api from '../../../infra/services/api';
 
 const Album: React.FC = () => {
+  const { modalGallery, setModalGallery, setIdAlbum, setName } = useContext(ModaGalleryContext);
   const [valueSearch, setValueSearch] = useState<string>("");
   const [albums, setAlbums] = useState<IAlbum[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [stateBtnLeft, setStateBtnLeft] = useState<boolean>(true);
+  const [stateBtnRight, setStateBtnRight] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
 
   useEffect(() => {
+    LoadAlbum();
+    statePage();
+  }, [page])
 
-  }, [])
+  const statePage = () => {
+    if (page === 1) {
+      setStateBtnLeft(true)
+    }
+    else if (page === 10) {
+      setStateBtnRight(true);
+    }
+    else {
+      setStateBtnLeft(false);
+      setStateBtnRight(false);
+    }
+  }
 
-  const LoadAlbum = () => {
-    api.get('/albums')
+  const LoadAlbum = useCallback(() => {
+    api.get(`/albums?userId=${page}`)
       .then(response => {
+        setLoading(false);
         setAlbums(response.data);
-        console.log(response.data);
+        setRefreshing(false);
       })
+      .catch(error => {
+        setLoading(false);
+        setRefreshing(false);
+        console.log("error")
+      })
+  }, [page])
+
+  const _onRefresh = () => {
+    setRefreshing(true);
+    LoadAlbum();
   }
 
   const renderItem: ListRenderItem<IAlbum> = ({ item }) => {
     return (
-      <>
-      </>
+      <ContainerCards>
+        <BtnCards onPress={() => { setIdAlbum(item.id), setName(item.title), setModalGallery(true) }}>
+          <Cards />
+        </BtnCards>
+
+        <TxtTitle>{item.title}</TxtTitle>
+      </ContainerCards>
     )
   }
 
@@ -43,24 +97,54 @@ const Album: React.FC = () => {
   }
   return (
     <>
+      <ModalGallery />
       <StatusBar backgroundColor={"#E5E5E5"} />
       <Container>
-        <Header />
+        <ScroolView
+          keyboardShouldPersistTaps='handled'
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={_onRefresh.bind(this)}
+            />
+          }
+        >
+          <Header />
 
-        <SearchBarPost
-          placeholder="Digite o título"
-          onChangeText={value => { setValueSearch(String(value)), SearchPost(value) }}
-          value={valueSearch}
-        />
+          <SearchBarPost
+            placeholder="Digite o título"
+            onChangeText={value => { setValueSearch(String(value)), SearchPost(value) }}
+            value={valueSearch}
+          />
 
-        <FlatList
-          data={albums}
-          keyExtractor={album => String(album.id)}
-          showsVerticalScrollIndicator={false}
-          numColumns={3}
-          renderItem={renderItem}
-        />
+          {loading
+            ? <Loading />
 
+            : <ViewPagination>
+              <TxtPage>{page} de 10</TxtPage>
+              <BtnLeft
+                disabled={stateBtnLeft}
+                state={stateBtnLeft}
+                onPress={() => { setPage(page - 1), setStateBtnLeft(false), setLoading(true) }}>
+                <IconLeft name="chevron-left" size={30} />
+              </BtnLeft>
+              <BtnRight
+                disabled={stateBtnRight}
+                state={stateBtnRight}
+                onPress={() => { setPage(page + 1), setStateBtnLeft(false), setLoading(true) }}>
+                <IconRight name="chevron-right" size={30} />
+              </BtnRight>
+            </ViewPagination>
+          }
+
+          <FlatList
+            data={albums}
+            keyExtractor={album => String(album.id)}
+            showsVerticalScrollIndicator={false}
+            numColumns={3}
+            renderItem={renderItem}
+          />
+        </ScroolView>
       </Container>
     </>
   )
