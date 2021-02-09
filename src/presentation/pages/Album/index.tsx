f5ed27df602e8a637ad96a7f9c47653b9a180aa7
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-09 11:16:49 -0400

    fix: adding error component

--- src/presentation/pages/Album/index.tsx
+++ src/presentation/pages/Album/index.tsx
@@ -30,12 +30,17 @@ import {
 
 import Loading from '../../components/Loading';
 
+import Error from '../../components/Error';
+
 import { IAlbum } from '../../../data/protocols/Album';
+
+import ErrorContext from '../../../data/contexts/Error';
 
 import api from '../../../infra/services/api';
 
 const Album: React.FC = () => {
   const { modalGallery, setModalGallery, setIdAlbum, setName, setStateModalLoading } = useContext(ModaGalleryContext);
+  const { pressTry } = useContext(ErrorContext);
   const [valueSearch, setValueSearch] = useState<string>("");
   const [albums, setAlbums] = useState<IAlbum[]>([]);
   const [albumsCopy, setAlbumsCopy] = useState<IAlbum[]>([]);
@@ -44,11 +49,19 @@ const Album: React.FC = () => {
   const [stateBtnRight, setStateBtnRight] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
   const [refreshing, setRefreshing] = useState<boolean>(false);
+  const [con, setCon] = useState<Boolean>(true);
 
   useEffect(() => {
     LoadAlbum();
     statePage();
   }, [page])
+
+  useEffect(() => {
+    if (pressTry === 3) {
+      LoadAlbum();
+      statePage();
+    }
+  }, [pressTry])
 
   const statePage = () => {
     if (page === 1) {
@@ -68,10 +81,12 @@ const Album: React.FC = () => {
       .then(response => {
         setLoading(false);
         setAlbums(response.data);
+        setCon(true);
         setAlbumsCopy(response.data);
         setRefreshing(false);
       })
       .catch(error => {
+        setCon(false);
         setLoading(false);
         setRefreshing(false);
       })
@@ -111,64 +126,70 @@ const Album: React.FC = () => {
       <ModalGallery />
       <StatusBar backgroundColor={"#E5E5E5"} />
       <Container>
-        <ScroolView
-          keyboardShouldPersistTaps='handled'
-          refreshControl={
-            <RefreshControl
-              refreshing={refreshing}
-              onRefresh={_onRefresh.bind(this)}
-            />
-          }
-        >
-          <Header />
+        {
+          con
+            ?
+            <ScroolView
+              keyboardShouldPersistTaps='handled'
+              refreshControl={
+                <RefreshControl
+                  refreshing={refreshing}
+                  onRefresh={_onRefresh.bind(this)}
+                />
+              }
+            >
+              <Header />
 
-          <SearchBarPost
-            placeholder="Digite o título"
-            onChangeText={value => { setValueSearch(value), searchPost(value) }}
-            value={valueSearch}
-          />
+              <SearchBarPost
+                placeholder="Digite o título"
+                onChangeText={value => { setValueSearch(value), searchPost(value) }}
+                value={valueSearch}
+              />
 
-          {loading
-            ? <Loading  clr={1}/>
+              {loading
+                ? <Loading clr={1} />
 
-            : <ViewPagination>
-              <TxtPage>{page} de 10</TxtPage>
-              <BtnLeft
-                disabled={stateBtnLeft}
-                state={stateBtnLeft}
-                onPress={() => { setPage(page - 1), setStateBtnLeft(false), setLoading(true) }}>
-                <IconLeft name="chevron-left" size={30} />
-              </BtnLeft>
-              <BtnRight
-                disabled={stateBtnRight}
-                state={stateBtnRight}
-                onPress={() => { setPage(page + 1), setStateBtnLeft(false), setLoading(true) }}>
-                <IconRight name="chevron-right" size={30} />
-              </BtnRight>
-            </ViewPagination>
-          }
+                : <ViewPagination>
+                  <TxtPage>{page} de 10</TxtPage>
+                  <BtnLeft
+                    disabled={stateBtnLeft}
+                    state={stateBtnLeft}
+                    onPress={() => { setPage(page - 1), setStateBtnLeft(false), setLoading(true) }}>
+                    <IconLeft name="chevron-left" size={30} />
+                  </BtnLeft>
+                  <BtnRight
+                    disabled={stateBtnRight}
+                    state={stateBtnRight}
+                    onPress={() => { setPage(page + 1), setStateBtnLeft(false), setLoading(true) }}>
+                    <IconRight name="chevron-right" size={30} />
+                  </BtnRight>
+                </ViewPagination>
+              }
 
-          <SafeAreaView>
-            {
-              albums.map((item, index) => {
-                return (
-                  <ContainerCards key={index} >
-                    <BtnCards onPress={() => {
-                      setIdAlbum(item.id),
-                        setName(item.title),
-                        setModalGallery(true),
-                        setTimeout(() => { setStateModalLoading(true) }, 50)
-                    }}>
-                      <Cards>
-                        <TxtTitle>{item.title}</TxtTitle>
-                      </Cards>
-                    </BtnCards>
-                  </ContainerCards>
-                )
-              })
-            }
-          </SafeAreaView>
-        </ScroolView>
+              <SafeAreaView>
+                {
+                  albums.map((item, index) => {
+                    return (
+                      <ContainerCards key={index} >
+                        <BtnCards onPress={() => {
+                          setIdAlbum(item.id),
+                            setName(item.title),
+                            setModalGallery(true),
+                            setTimeout(() => { setStateModalLoading(true) }, 50)
+                        }}>
+                          <Cards>
+                            <TxtTitle>{item.title}</TxtTitle>
+                          </Cards>
+                        </BtnCards>
+                      </ContainerCards>
+                    )
+                  })
+                }
+              </SafeAreaView>
+            </ScroolView>
+            : <Error opt={3} />
+        }
+
       </Container>
     </>
   )
