f56cdde580c28824598867cc6ef6dfa2672b3f7c
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-08 21:07:38 -0400

    fix: adding modal loading

--- src/presentation/components/ModalGallery/index.tsx
+++ src/presentation/components/ModalGallery/index.tsx
@@ -15,18 +15,24 @@ import {
   ContainerCards,
   BtnBack,
   IconBack,
-  ViewBack
+  ViewBack,
+  ContainerLoading,
+  TxtLoading
 } from './styles';
 
 import api from '../../../infra/services/api';
 
 import { Alert, ListRenderItem } from 'react-native';
 
+import Loading from '../Loading';
+
 import { IPhoto } from '../../../data/protocols/ModalGallery';
 
 const ModalGallery: React.FC = () => {
   const { modalGallery, setModalGallery, idAlbum, setIdAlbum, name } = useContext(ModalGalleryContext);
   const [photos, setPhotos] = useState<IPhoto[]>([]);
+  const [stateLoading, setStateLoading] = useState<Boolean>(true);
+  const [stateModalLoading, setStateModalLoading] = useState<boolean>(false);
 
   useEffect(() => {
     loadGallery();
@@ -38,21 +44,40 @@ const ModalGallery: React.FC = () => {
         setPhotos(response.data);
       })
       .catch(error => {
+        setStateModalLoading(false);
         console.log("Error no modalGallery")
       })
   }, [idAlbum, modalGallery])
+
+  const ModalLoading = () => {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={stateModalLoading}
+        onRequestClose={() => {
+          setStateModalLoading(false);
+        }}
+      >
+        <ContainerLoading>
+          <TxtLoading>Carregando...</TxtLoading>
+          <Loading />
+        </ContainerLoading>
+      </Modal>
+    )
+  }
 
   const renderItem: ListRenderItem<IPhoto> = ({ item }) => {
     return (
       <ContainerCards>
         <BtnImg>
-          <Img 
-            source={{ uri: `${item.thumbnailUrl}` }} 
-            onLoad={e => {}} 
-            onError={e => { }} 
+          <Img
+            source={{ uri: `${item.thumbnailUrl}` }}
+            onLoad={e => { setStateLoading(false), setStateModalLoading(false) }}
+            onLoadStart={() => { setStateLoading(true), setStateModalLoading(true) }}
+            onError={e => { setStateLoading(false), setStateModalLoading(false) }}
             resizeMode="stretch"
           >
-            
           </Img>
         </BtnImg>
       </ContainerCards>
@@ -60,34 +85,38 @@ const ModalGallery: React.FC = () => {
   }
 
   return (
-    <Container>
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalGallery}
-        onRequestClose={() => {
-          setPhotos([]);
-          setModalGallery(false);
-        }}
-      >
-        <ScroolView>
-        <ViewBack>
-          <BtnBack onPress={() => {setPhotos([]); setModalGallery(false)}}>
-            <IconBack name="arrow-left" size={35} />
-          </BtnBack>
-          <TxtTitleScreen>{name}</TxtTitleScreen>
-        </ViewBack>
-        
-        <FlatList
-          data={photos}
-          keyExtractor={photo => String(photo.id)}
-          showsVerticalScrollIndicator={false}
-          numColumns={3}
-          renderItem={renderItem}
-        />
-        </ScroolView>
-      </Modal>
-    </Container>
+    <>
+    {ModalLoading()}
+      <Container>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalGallery}
+          onRequestClose={() => {
+            setPhotos([]);
+            setStateModalLoading(false);
+            setModalGallery(false);
+          }}
+        >
+          <ScroolView>
+            <ViewBack>
+              <BtnBack onPress={() => { setPhotos([]); setModalGallery(false) }}>
+                <IconBack name="arrow-left" size={35} />
+              </BtnBack>
+              <TxtTitleScreen>{name}</TxtTitleScreen>
+            </ViewBack>
+
+            <FlatList
+              data={photos}
+              keyExtractor={photo => String(photo.id)}
+              showsVerticalScrollIndicator={false}
+              numColumns={4}
+              renderItem={renderItem}
+            />
+          </ScroolView>
+        </Modal>
+      </Container>
+    </>
   )
 }
 
