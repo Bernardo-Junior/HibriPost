ee629eba7bf3fccc601bd9049eab73e35387e4da
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-10 00:39:49 -0400

    fix: changing type of parameter size

--- src/presentation/responsivity/index.ts
+++ src/presentation/responsivity/index.ts
@@ -5,7 +5,7 @@ export const { width } = Dimensions.get('window');
 // based on Pixel 2 XL's scale
 const scale = width / 411;
 
-const normalize = (size: any) => {
+const normalize = (size: number) => {
   const newSize = size * scale;
 
   return parseFloat(PixelRatio.roundToNearestPixel(newSize).toFixed(2));
